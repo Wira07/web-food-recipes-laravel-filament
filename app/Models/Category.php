633247fee8d1590062c50fa3627420806cbdfce2
Wira07 <wiralodrasaputra07@gmail.com>
2024-09-08 13:43:24 +0700

    Refactor Filament resource pages and models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Str; // Tambahkan ini untuk helper Str
+use Illuminate\Support\Str;
 
 class Category extends Model
 {
@@ -15,15 +15,17 @@ class Category extends Model
     protected $fillable = [
         'name',
         'slug',
-        'icon', // Perbaikan typo 'icom' menjadi 'icon'
+        'icon',
     ];
 
+    // Setter for the 'name' attribute
     public function setNameAttribute($value)
     {
         $this->attributes['name'] = $value;
-        $this->attributes['slug'] = Str::slug($value); // Gunakan Str::slug
+        $this->attributes['slug'] = Str::slug($value);
     }
 
+    // One-to-many relationship with Recipe model
     public function recipes(): HasMany
     {
         return $this->hasMany(Recipe::class);
